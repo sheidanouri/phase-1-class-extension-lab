d8dae70c4594d5ca573344c3d9edd7c0a14c687d
sheida noori <sheida.nouri@gmail.com>
2023-04-08 10:50:08 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+class Polygon{
+     
+    constructor(side){
+        this.side = side
+
+    }
+
+    get countSides(){
+       return this.side.length
+    }
+
+    get perimeter(){
+        let sum = 0
+        for(let i =0 ; i<this.side.length; i++){
+            
+             sum +=this.side[i]
+        }
+        return sum
+    }
+}
+
+class Triangle extends Polygon{
+
+   get isValid(){
+    return this.countSides === 3 && 
+    this.side[0]<this.side[1] + this.side[2] &&
+    this.side[2]<this.side[1] + this.side[0] &&
+    this.side[1]<this.side[0] + this.side[2]
+ 
+}
+}
+
+class Square extends Polygon {
+
+    get isValid (){
+
+        return this.countSides === 4 
+        && this.side[0]===this.side[1] &&
+         this.side[2]===this.side[3]&&
+         this.side[3]===this.side[0]
+    }
+    get area(){
+        return this.side[0] * this.side[0]
+    }
+
+}
